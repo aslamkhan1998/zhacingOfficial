fb2118c38846a81f3ff6fb7cb01a0b4835402123
aslamkhan1998 <67847981+aslamkhan1998@users.noreply.github.com>
2020-09-16 14:51:34 +0800

    Updates

---
+++ precache-manifest.e555bea175e8ef59979eb8a844bae7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22009cbeca04f6faeb6fd384371ff4e",
+    "url": "/zhacingOfficial/index.html"
+  },
+  {
+    "revision": "c67eea2c10a6ac5491d1",
+    "url": "/zhacingOfficial/static/css/2.b3d9b814.chunk.css"
+  },
+  {
+    "revision": "61a9e7c2e368a91978c9",
+    "url": "/zhacingOfficial/static/css/main.27e77091.chunk.css"
+  },
+  {
+    "revision": "c67eea2c10a6ac5491d1",
+    "url": "/zhacingOfficial/static/js/2.f22a5368.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/zhacingOfficial/static/js/2.f22a5368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a9e7c2e368a91978c9",
+    "url": "/zhacingOfficial/static/js/main.d6c2d0c3.chunk.js"
+  },
+  {
+    "revision": "2b65e8f4eda10ef0abe3",
+    "url": "/zhacingOfficial/static/js/runtime-main.7884c2c9.js"
+  }
+]);
